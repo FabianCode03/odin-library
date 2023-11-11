708090fec535cfcbf98c6355d6b59eb6d6e42769
Fabian_Laptop <fb-224651@hs-weingarten.de>
2023-11-11 15:53:24 +0100

    Implement pushing books with form values

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
+// selectors and variable definitions
 const myLibrary = [];
-
-function Book(title, author, numberOfPages) {
-  this.title = title;
-  this.author = author;
-  this.numberOfPages = numberOfPages;
-}
-
-function addBookToLibrary() {
-  const title = prompt("Please enter the Title of the new Book");
-  const author = prompt("Please enter the Author of the new Book");
-  const pages = prompt("Please enter the Number of pages of the new Book");
-  const newBook = new Book(title, author, pages);
-  myLibrary.push(newBook);
-}
-
-// addBookToLibrary();
-//console.table(myLibrary);
-
 const modal = document.querySelector(".modal");
 const openModal = document.querySelector(".open-modal");
 const closeModal = document.querySelector(".cancel");
+const submitButton = document.querySelector(".add");
+const form = document.querySelector("#form");
 
+// constructor
+function Book(title, author, numberOfPages, read) {
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.read = read;
+}
+
+function addBook(e) {
+  e.preventDefault();
+
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let numberOfPages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+
+  const newBook = new Book(title, author, numberOfPages, read);
+  myLibrary.push(newBook);
+  console.table(myLibrary);
+}
+// event listeners
+submitButton.addEventListener("click", e => addBook(e));
 openModal.addEventListener("click", () => modal.showModal());
 closeModal.addEventListener("click", () => modal.close());
