e55daade4fa2798c4163d7e8e56489c98d3c10e3
Fabian_Laptop <fb-224651@hs-weingarten.de>
2023-11-11 19:00:23 +0100

    Style books and add them to book-display

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const openModal = document.querySelector(".open-modal");
 const closeModal = document.querySelector(".cancel");
 const submitButton = document.querySelector(".add");
 const form = document.querySelector("#form");
+const toggleRead = document.querySelectorAll(".read");
+const bookDisplay = document.querySelector(".book-display");
 
 // constructor
 function Book(title, author, numberOfPages, read) {
@@ -24,8 +26,45 @@ function addBook(e) {
 
   const newBook = new Book(title, author, numberOfPages, read);
   myLibrary.push(newBook);
-  console.table(myLibrary);
+  addBookToDom(newBook);
+  form.reset();
+  modal.close();
 }
+
+function changeReadStatus(e) {
+  if (e.target.classList.contains("read")) {
+    e.target.classList.remove("read");
+    e.target.classList.add("notRead");
+    e.target.textContent = "Not Read";
+  } else {
+    e.target.classList.remove("notRead");
+    e.target.classList.add("read");
+    e.target.textContent = "Read";
+  }
+}
+
+function addBookToDom(book) {
+  let bookTemplate = `
+  <div class="book">
+  <h1 class="book-title">${book.title}</h1>
+  <p class="book-author">Author: ${book.author}</p>
+  <p class="book-pages">Pages: ${book.numberOfPages}</p>
+  <button class="${book.read ? "read" : "notRead"}">${
+    book.read ? "Read" : "Not Read"
+  }</button>
+  <button class="removeBook">Remove</button>
+  </div>
+`;
+
+  // Fügen Sie das Buch-Template als neues Kind-Element ein
+  bookDisplay.insertAdjacentHTML("beforeend", bookTemplate);
+
+  // Fügen Sie den Event-Listener zum zuletzt hinzugefügten "Read"-Button hinzu
+  let readButtons = document.querySelectorAll(".read, .notRead");
+  let lastReadButton = readButtons[readButtons.length - 1];
+  lastReadButton.addEventListener("click", e => changeReadStatus(e));
+}
+
 // event listeners
 submitButton.addEventListener("click", e => addBook(e));
 openModal.addEventListener("click", () => modal.showModal());
