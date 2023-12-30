b38b0a19bfec6bea088ee60ed0d5f19b7d6360cb
Fabian_PC <fb-224651@hs-weingarten.de>
2023-12-30 11:20:50 +0100

    Move methods in their classes

--- script.js
+++ script.js
@@ -3,8 +3,37 @@ class Library {
 
   constructor() {}
 
-  addBook() {}
-  removeBook(e) {}
+  addBook() {
+    e.preventDefault();
+
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let numberOfPages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+
+    const newBook = new Book(title, author, numberOfPages, read);
+    myLibrary.push(newBook);
+    addBookToDom(newBook);
+    form.reset();
+    modal.close();
+    console.log(myLibrary);
+  }
+  removeBook(e) {
+    // Finden Sie das Buch-Element im DOM
+    let bookElement = e.target.parentNode;
+
+    // Finden Sie den Index des Buches im Array
+    let index = Array.from(bookElement.parentNode.children).indexOf(
+      bookElement
+    );
+
+    // Entfernen Sie das Buch aus dem Array
+    myLibrary.splice(index, 1);
+
+    // Entfernen Sie das Buch aus dem DOM
+    bookElement.remove();
+    console.log(myLibrary);
+  }
 }
 
 class Book {
@@ -15,7 +44,17 @@ class Book {
     this.read = read;
   }
 
-  changeReadStatus() {}
+  changeReadStatus() {
+    if (e.target.classList.contains("read")) {
+      e.target.classList.remove("read");
+      e.target.classList.add("notRead");
+      e.target.textContent = "Not Read";
+    } else {
+      e.target.classList.remove("notRead");
+      e.target.classList.add("read");
+      e.target.textContent = "Read";
+    }
+  }
 }
 
 class Displaycontroller {
@@ -31,6 +70,32 @@ class Displaycontroller {
     submitButton.addEventListener("click", (e) => addBook(e));
     openModal.addEventListener("click", () => modal.showModal());
     closeModal.addEventListener("click", () => modal.close());
+  }
+
+  addBookToDom(book) {
+    let bookTemplate = `
+    <div class="book">
+    <h1 class="book-title">${book.title}</h1>
+    <p class="book-author">Author: ${book.author}</p>
+    <p class="book-pages">Pages: ${book.numberOfPages}</p>
+    <button class="${book.read ? "read" : "notRead"}">${
+      book.read ? "Read" : "Not Read"
+    }</button>
+    <button class="removeBook">Remove</button>
+    </div>
+  `;
+
+    // Fügen Sie das Buch-Template als neues Kind-Element ein
+    bookDisplay.insertAdjacentHTML("beforeend", bookTemplate);
+
+    // Fügen Sie den Event-Listener zum zuletzt hinzugefügten "Read"-Button hinzu
+    let readButtons = document.querySelectorAll(".read, .notRead");
+    let lastReadButton = readButtons[readButtons.length - 1];
+    lastReadButton.addEventListener("click", (e) => changeReadStatus(e));
+
+    let removeButtons = document.querySelectorAll(".removeBook");
+    let lastRemoveButton = removeButtons[removeButtons.length - 1];
+    lastRemoveButton.addEventListener("click", (e) => removeBook(e));
   }
 }
 
@@ -52,71 +117,45 @@ class Displaycontroller {
 //   this.read = read;
 // }
 
-function addBook(e) {
-  e.preventDefault();
+// function addBook(e) {
+//   e.preventDefault();
 
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let numberOfPages = document.getElementById("pages").value;
-  let read = document.getElementById("read").checked;
+//   let title = document.getElementById("title").value;
+//   let author = document.getElementById("author").value;
+//   let numberOfPages = document.getElementById("pages").value;
+//   let read = document.getElementById("read").checked;
 
-  const newBook = new Book(title, author, numberOfPages, read);
-  myLibrary.push(newBook);
-  addBookToDom(newBook);
-  form.reset();
-  modal.close();
-  console.log(myLibrary);
-}
+//   const newBook = new Book(title, author, numberOfPages, read);
+//   myLibrary.push(newBook);
+//   addBookToDom(newBook);
+//   form.reset();
+//   modal.close();
+//   console.log(myLibrary);
+// }
 
-function changeReadStatus(e) {
-  if (e.target.classList.contains("read")) {
-    e.target.classList.remove("read");
-    e.target.classList.add("notRead");
-    e.target.textContent = "Not Read";
-  } else {
-    e.target.classList.remove("notRead");
-    e.target.classList.add("read");
-    e.target.textContent = "Read";
-  }
-}
+// function changeReadStatus(e) {
+//   if (e.target.classList.contains("read")) {
+//     e.target.classList.remove("read");
+//     e.target.classList.add("notRead");
+//     e.target.textContent = "Not Read";
+//   } else {
+//     e.target.classList.remove("notRead");
+//     e.target.classList.add("read");
+//     e.target.textContent = "Read";
+//   }
+// }
 
-function addBookToDom(book) {
-  let bookTemplate = `
-  <div class="book">
-  <h1 class="book-title">${book.title}</h1>
-  <p class="book-author">Author: ${book.author}</p>
-  <p class="book-pages">Pages: ${book.numberOfPages}</p>
-  <button class="${book.read ? "read" : "notRead"}">${
-    book.read ? "Read" : "Not Read"
-  }</button>
-  <button class="removeBook">Remove</button>
-  </div>
-`;
+// function removeBook(e) {
+//   // Finden Sie das Buch-Element im DOM
+//   let bookElement = e.target.parentNode;
 
-  // Fügen Sie das Buch-Template als neues Kind-Element ein
-  bookDisplay.insertAdjacentHTML("beforeend", bookTemplate);
+//   // Finden Sie den Index des Buches im Array
+//   let index = Array.from(bookElement.parentNode.children).indexOf(bookElement);
 
-  // Fügen Sie den Event-Listener zum zuletzt hinzugefügten "Read"-Button hinzu
-  let readButtons = document.querySelectorAll(".read, .notRead");
-  let lastReadButton = readButtons[readButtons.length - 1];
-  lastReadButton.addEventListener("click", (e) => changeReadStatus(e));
+//   // Entfernen Sie das Buch aus dem Array
+//   myLibrary.splice(index, 1);
 
-  let removeButtons = document.querySelectorAll(".removeBook");
-  let lastRemoveButton = removeButtons[removeButtons.length - 1];
-  lastRemoveButton.addEventListener("click", (e) => removeBook(e));
-}
-
-function removeBook(e) {
-  // Finden Sie das Buch-Element im DOM
-  let bookElement = e.target.parentNode;
-
-  // Finden Sie den Index des Buches im Array
-  let index = Array.from(bookElement.parentNode.children).indexOf(bookElement);
-
-  // Entfernen Sie das Buch aus dem Array
-  myLibrary.splice(index, 1);
-
-  // Entfernen Sie das Buch aus dem DOM
-  bookElement.remove();
-  console.log(myLibrary);
-}
+//   // Entfernen Sie das Buch aus dem DOM
+//   bookElement.remove();
+//   console.log(myLibrary);
+// }
